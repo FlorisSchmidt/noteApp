3ac33f294cea29a0f1c6371e595c08aa4c9bda06
Floris Schmidt <floris.schmidt@hotmail.com>
2017-09-13 19:20:12 +0200

    Imported NoteList and NoteText components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NoteListComponent } from './noteList/noteList.component';
+import { NoteTextComponent } from './noteText/noteText.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent,
+    NoteTextComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
