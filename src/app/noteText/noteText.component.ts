b63c98002649f4f9ad8cf762c6e6ce8f85d0ddaf
Floris Schmidt <floris.schmidt@hotmail.com>
2017-09-20 17:49:56 +0200

    Added save and edit function

--- src/app/noteText/noteText.component.ts
+++ src/app/noteText/noteText.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
 import { NoteListComponent } from '../noteList/noteList.component';
-
 import { DataService } from '../data.service';
 
 @Component({
@@ -12,11 +10,19 @@ import { DataService } from '../data.service';
 export class NoteTextComponent implements OnInit {
 
   constructor(private DataService: DataService) {
-   }
+  }
+  private editable: boolean = false;
+  private button: string = 'EDIT';
 
   ngOnInit() {
   }
-  save() {
 
+  save(){
+    localStorage.setItem(localStorage.key(this.DataService.activeIndex), this.DataService.activeContent);
+  }
+
+  edit(){
+    this.editable ? this.editable=false : this.editable=true;
+    this.editable ? this.button='SAVE' : this.button='EDIT';
   }
 }
