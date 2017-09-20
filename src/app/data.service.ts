761f815ea9111df0c177733d68446ff29085df60
Floris Schmidt <floris.schmidt@hotmail.com>
2017-09-20 17:38:34 +0200

    Added activeContent variable

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,5 +4,6 @@ import { Injectable } from '@angular/core';
 export class DataService {
 
   constructor() { }
-  activeIndex: number;
+  public activeIndex: number;
+  public activeContent: string;
 }
