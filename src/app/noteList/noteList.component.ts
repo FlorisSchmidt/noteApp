94ff5ac4149e9dcccbb2713685af60dbfb25437f
Floris Schmidt <floris.schmidt@hotmail.com>
2017-09-20 17:49:00 +0200

    ## Added functions -List item will be active on startup -Removed notesContent array -Removed key string -Removed unnecessary parameter in addNote function -After remove a note, the index will change -Activecontent will change on click

--- src/app/noteList/noteList.component.ts
+++ src/app/noteList/noteList.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DataService } from '../data.service';
+
 @Component({
   selector: 'app-noteList',
   templateUrl: './noteList.component.html',
@@ -10,39 +11,50 @@ export class NoteListComponent implements OnInit {
   constructor(private DataService: DataService) {
     for(let x=0;x<localStorage.length;x++){
       this.notes[x]=localStorage.key(x);
-      this.notesContent[x]=localStorage.getItem(this.notes[x]);
+    }
+    if(localStorage.length==0){
+      this.DataService.activeIndex=undefined;
+    } else {
+      this.setActiveIndex(0);
     }
   }
 
   ngOnInit() {
   }
+
   public notes: string[]=[];
-  public notesContent=[];
-  private key: string = '';
   private inputValue: string = '';
 
-  addNote(key:string,content:string) {
-    if(key == ''){
+  addNote(input:string) {
+    if(input == ''){
       alert("Key is empty");
       return 1;
     }
-    if(localStorage.getItem(key)!=undefined){
+    if(localStorage.getItem(input)!=undefined){
       alert("It exists already!");
       return 2;
     }
-    this.notes.push(key);
-    localStorage.setItem(key,content);
+
+    this.notes.push(input);
     this.inputValue = '';
-    }
+    localStorage.setItem(input,"");
+    this.setActiveIndex(localStorage.length-1);
+  }
   
   removeNote(index:number) {
     let key=localStorage.key(index);
     localStorage.removeItem(key);
     this.notes.splice(index,1);
-    this.notesContent.splice(index,1)
+
+    if(localStorage.length>0){
+      this.setActiveIndex(0);
+    } else {
+      this.setActiveIndex(undefined);
+    }
   }
 
-  setActiveIndex(index){
+  setActiveIndex(index: number){
     this.DataService.activeIndex = index;
+    this.DataService.activeContent = localStorage.getItem(localStorage.key(this.DataService.activeIndex));
   }
 }
