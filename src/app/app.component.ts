170a3f716bbf912b74356083383f01e4ac6885e4
Floris Schmidt <floris.schmidt@hotmail.com>
2017-09-26 12:46:38 +0200

    Removed DataService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { NoteListComponent } from './noteList/noteList.component';
-import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [DataService]
+  providers: []
 })
 export class AppComponent {
   title='app';
