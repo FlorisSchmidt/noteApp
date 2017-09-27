b8cc145b3c373fd3666d6db1f6c7bfa6e6feeddd
Floris Schmidt <floris.schmidt@hotmail.com>
2017-09-27 17:15:20 +0200

    Added MarkPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { NoteListComponent } from './noteList/noteList.component';
 import { NoteTextComponent } from './noteText/noteText.component';
 import { RouterModule } from '@angular/router';
-
+import { MarkPipe } from './mark.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NoteListComponent,
-    NoteTextComponent
+    NoteTextComponent,
+    MarkPipe
   ],
   imports: [
     BrowserModule,
